3da9def256078c1a5d6f978de9b726fc4af74fbb
AntonReinhard <anton.reinhard@proton.me>
2025-04-20 19:36:13 +0200

    Add abs * dm * bs sandwich gpu test

--- test/gpu/tensor_multiplication.jl
+++ test/gpu/tensor_multiplication.jl
@@ -22,8 +22,9 @@ const N = 256
 
 @testset "gpu tests for $VECTOR_T" for (GPU_MODULE, VECTOR_T) in GPUS
     @testset "float type $FLOAT_T" for FLOAT_T in GPU_FLOAT_TYPES[GPU_MODULE]
+        COMPLEX_T = Complex{FLOAT_T}
+
         @testset "multiplication $T1 * $T2 = $T3" for (T1, T2, T3) in ALLOWED_MULS
-            COMPLEX_T = Complex{FLOAT_T}
 
             par1 = rand(RNG, T1{COMPLEX_T}, N)
             par2 = rand(RNG, T2{COMPLEX_T}, N)
@@ -35,6 +36,24 @@ const N = 256
 
             gpu_results = par1_gpu .* par2_gpu
 
+            @test eltype(gpu_results) == T3{COMPLEX_T}
+            @test all(isapprox.(Vector(gpu_results), gt_results))
+        end
+
+        @testset "multiplication AdjointBiSpinor * DiracMatrix * BiSpinor" begin
+            par1 = rand(RNG, AdjointBiSpinor{COMPLEX_T}, N)
+            par2 = rand(RNG, DiracMatrix{COMPLEX_T}, N)
+            par3 = rand(RNG, BiSpinor{COMPLEX_T}, N)
+
+            gt_results = par1 .* par2 .* par3
+
+            par1_gpu = VECTOR_T(par1)
+            par2_gpu = VECTOR_T(par2)
+            par3_gpu = VECTOR_T(par3)
+
+            gpu_results = par1_gpu .* par2_gpu .* par3_gpu
+
+            @test eltype(gpu_results) == COMPLEX_T
             @test all(isapprox.(Vector(gpu_results), gt_results))
         end
     end
