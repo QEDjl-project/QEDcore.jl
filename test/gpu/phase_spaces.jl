c185a1f675c4b71008b776b5f667c3dc18aa7057
AntonReinhard <anton.reinhard@proton.me>
2025-04-23 00:25:50 +0200

    Add test building psp from coordinates
    CI_UNIT_PKG_URL_QEDbase: https://github.com/AntonReinhard/QEDbase.jl#precision

--- test/gpu/phase_spaces.jl
+++ test/gpu/phase_spaces.jl
@@ -6,7 +6,6 @@ RNG = Random.MersenneTwister(573)
 N = 256
 
 TESTMODEL = TestImplementation.TestModel()
-TESTPSL = TestImplementation.TestOutPhaseSpaceLayout()
 
 @testset "phase space tests for $VECTOR_T" for (GPU_MODULE, VECTOR_T) in GPUS
     @testset "float type $FLOAT_T" for FLOAT_T in GPU_FLOAT_TYPES[GPU_MODULE]
@@ -29,17 +28,46 @@ TESTPSL = TestImplementation.TestOutPhaseSpaceLayout()
         gpu_out_phs = VECTOR_T(out_phs)
 
         model = TESTMODEL
-        process = TestImplementation.TestProcess((Electron(), Photon()), (Electron(), Photon()))
-        psl = TESTPSL
+        proc = TestImplementation.TestProcess((Electron(), Photon()), (Electron(), Photon()))
+        in_psl = TestImplementation.TestInPhaseSpaceLayout()
+        psl = FlatPhaseSpaceLayout(in_psl)
 
-        psps = PhaseSpacePoint.(process, model, psl, tuple.(in_els, in_phs), tuple.(out_els, out_phs))
-        gpu_psps = PhaseSpacePoint.(process, model, psl, tuple.(gpu_in_els, gpu_in_phs), tuple.(gpu_out_els, gpu_out_phs))
+        psps = PhaseSpacePoint.(proc, model, Ref(psl), tuple.(in_els, in_phs), tuple.(out_els, out_phs))
+        gpu_psps = PhaseSpacePoint.(proc, model, Ref(psl), tuple.(gpu_in_els, gpu_in_phs), tuple.(gpu_out_els, gpu_out_phs))
 
-        @testset "Accessors" begin
+        @testset "accessors" begin
             @test sum(isapprox.(Vector(momentum.(gpu_psps, Incoming(), Electron())), in_el_moms)) == N
             @test sum(isapprox.(Vector(momentum.(gpu_psps, Incoming(), Photon())), in_ph_moms)) == N
             @test sum(isapprox.(Vector(momentum.(gpu_psps, Outgoing(), Electron())), out_el_moms)) == N
             @test sum(isapprox.(Vector(momentum.(gpu_psps, Outgoing(), Photon())), out_ph_moms)) == N
+
+            @test sum(isapprox.(Vector(momentum.(gpu_psps, Incoming(), Val(1))), in_el_moms)) == N
+            @test sum(isapprox.(Vector(momentum.(gpu_psps, Incoming(), Val(2))), in_ph_moms)) == N
+            @test sum(isapprox.(Vector(momentum.(gpu_psps, Outgoing(), Val(1))), out_el_moms)) == N
+            @test sum(isapprox.(Vector(momentum.(gpu_psps, Outgoing(), Val(2))), out_ph_moms)) == N
+        end
+
+        @testset "generation from coordinates" begin
+            IN_MASSES = mass.(incoming_particles(proc))
+            SUM_IN_MASSES = sum(IN_MASSES)
+            OUT_MASSES = mass.(outgoing_particles(proc))
+            SUM_OUT_MASSES = sum(OUT_MASSES)
+            TESTSQRTS = [(one(FLOAT_T) + rand(RNG, FLOAT_T)) * (SUM_OUT_MASSES + SUM_IN_MASSES) for _ in 1:N]
+            TESTINMOMS = TestImplementation._generate_onshell_two_body_moms.(
+                Ref(RNG), Ref(IN_MASSES), TESTSQRTS
+            )
+            test_out_psl = FlatPhaseSpaceLayout(in_psl)
+            TESTOUTCOORDS = [Tuple(rand(RNG, 4 * number_outgoing_particles(proc))) for _ in 1:N]
+
+            test_out_moms = QEDbase.build_momenta.(
+                proc, model, TESTINMOMS, Ref(test_out_psl), TESTOUTCOORDS
+            )
+
+            test_out_moms_gpu = QEDbase.build_momenta.(
+                proc, model, VECTOR_T(TESTINMOMS), Ref(test_out_psl), VECTOR_T(TESTOUTCOORDS)
+            )
+
+            @test sum(isapprox.(Vector(gpu_test_out_moms), test_out_moms)) == N
         end
     end
 end
